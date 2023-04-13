f399bd6dcd393dd748fe2e5ab6fa56ec389d0391
nailul27 <78847062+nailul27@users.noreply.github.com>
2023-04-13 12:41:55 +0700

    second

--- lib/details/detail_screen.dart
+++ lib/details/detail_screen.dart
@@ -1,42 +1,133 @@
+import 'package:biodata/widgets/header_widget.dart';
 import 'package:flutter/material.dart';
 
-class Detail extends StatelessWidget {
+class Detail extends StatefulWidget {
   const Detail({super.key});
+
+  @override
+  State<Detail> createState() => _DetailState();
+}
+
+class _DetailState extends State<Detail> {
+
+  double  _drawerIconSize = 24;
+  double _drawerFontSize = 17;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('My biodata detail'),
+      appBar: AppBar(
+        title: Text("Profile Page",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
-        body: Container(
-          padding: EdgeInsets.only(left: 10, top: 20, right: 10),
-          child: ListView(
-            children: [
-              Center(
-                child: Stack(
-                  children: [
-                    Container(
-                      width: 130,
-                      height: 130,
-                      decoration: BoxDecoration(
-                          border: Border.all(width: 4, color: Colors.white),
-                          boxShadow: [
-                            BoxShadow(
-                                spreadRadius: 2,
-                                blurRadius: 10,
-                                color: Colors.black.withOpacity(0.1))
-                          ],
-                          shape: BoxShape.circle,
-                          image: DecorationImage(fit: BoxFit.cover, image:AssetImage('assets/profile.png'))
-                          ),
-                    )
-                  ],
-                ),
-              ),
-              Text('Data diri saya')
-            ],
+        elevation: 0.5,
+        iconTheme: IconThemeData(color: Colors.white),
+        flexibleSpace:Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: <Color>[Theme.of(context).primaryColor, Theme.of(context).accentColor,]
+              )
           ),
-        ));
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Container(height: 100, child: HeaderWidget(100,false,Icons.house_rounded),),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.fromLTRB(25, 10, 25, 10),
+              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      border: Border.all(width: 5, color: Colors.white),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(color: Colors.black12, blurRadius: 20, offset: const Offset(5, 5),),
+                      ],
+                    ),
+                    child: Icon(Icons.person, size: 80, color: Colors.grey.shade300,),
+                  ),
+                  SizedBox(height: 20,),
+                  Text('M Fasih Arobi', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),),
+                  SizedBox(height: 20,),
+                  Text('Programmer', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                  SizedBox(height: 10,),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          padding: const EdgeInsets.only(left: 8.0, bottom: 4.0),
+                          alignment: Alignment.topLeft,
+                          child: Text(
+                            "Biodata Diri",
+                            style: TextStyle(
+                              color: Colors.black87,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                        Card(
+                          child: Container(
+                            alignment: Alignment.topLeft,
+                            padding: EdgeInsets.all(15),
+                            child: Column(
+                              children: <Widget>[
+                                Column(
+                                  children: <Widget>[
+                                    ...ListTile.divideTiles(
+                                      color: Colors.grey,
+                                      tiles: [
+                                        ListTile(
+                                          leading: Icon(Icons.person),
+                                          title: Text("Tentang saya"),
+                                          subtitle: Text(
+                                              "saya sedang belajar flutter"),
+                                        ),
+                                        ListTile(
+                                          contentPadding: EdgeInsets.symmetric(
+                                              horizontal: 12, vertical: 4),
+                                          leading: Icon(Icons.my_location),
+                                          title: Text("Alamat"),
+                                          subtitle: Text("Jember"),
+                                        ),
+                                        ListTile(
+                                          leading: Icon(Icons.email),
+                                          title: Text("Email"),
+                                          subtitle: Text("muhfasiharobi@gmail.com"),
+                                        ),
+                                        ListTile(
+                                          leading: Icon(Icons.phone),
+                                          title: Text("Phone"),
+                                          subtitle: Text("098-0111-0000"),
+                                        ),
+                                        
+                                      ],
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
